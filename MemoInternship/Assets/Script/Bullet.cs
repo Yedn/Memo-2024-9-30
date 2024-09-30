ea4272e3f7e1886f6202b6231266f9ed841a3bc1
龙门特产龙井茶 <tieyewang054@gmail.com>
2024-10-01 02:00:25 +0800

    move

---
+++ MemoInternship/Assets/Script/Bullet.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Bullet Seting")]
+    public float atkValue = 2.5f;
+    public float bulletSpeed = 5.0f;
+    public GameObject bulletDestoryPrefab;
+
+    public Vector2 shootPoint;
+
+    public void Start()
+    {
+        Destroy(bulletDestoryPrefab,9.0f);
+    }
+
+    public void SetatkValue(float atkValue)
+    {
+        this.atkValue = atkValue;
+    }
+
+    public void SetBulletSpeed(float bulletSpeed)
+    { 
+        this.bulletSpeed = bulletSpeed;
+    }
+
+    public void SetShootPoint(Vector2 shootPoint)
+    {
+        this.shootPoint = shootPoint;
+    }
+
+    private void Update()
+    {
+        transform.Translate(shootPoint * bulletSpeed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enamy")
+        {
+            Destroy(this.gameObject);
+            //Õâ±ßÒª×ö¿ÛÑª
+            if (collision.tag == "Tree")
+            {
+                Destroy(this.gameObject);
+            }
+            GameObject go = GameObject.Instantiate(bulletDestoryPrefab, transform.position, Quaternion.identity);
+            Destroy(go, 1.0f);
+        }
+    }
+}
---
+++ MemoInternship/Assets/Script/PlayerController.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Playerstate {game,die,pause}
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float speed = 5.0f;
+    public GameObject weapon;
+    public Playerstate playerstate = Playerstate.pause;
+    public float Hp = 5.0f;
+    public float atkValue = 2.5f;
+    public Bullet bulletPrefab;
+    public float bulletSpeed=5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = this.transform;
+        TranslateToGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(playerstate)
+        {
+            case Playerstate.game:
+                {
+                    GameingUpdate(); break;
+                }
+            case Playerstate.die:
+                {
+                    DieUpdate(); break;
+                }
+            case Playerstate.pause:
+                {
+
+                }
+                break;
+        }
+    }
+
+    public void TranslateToGame()
+    {
+        playerstate = Playerstate.game;
+    }
+
+    public void TranslateToDie()
+    {
+        playerstate = Playerstate.die;
+    }
+
+    public void DieUpdate()
+    {
+
+    }
+
+    public void GameingUpdate()
+    {
+        PlayerMove();
+        LookAt();
+        if (Hp <= 0 )
+        {
+            TranslateToDie();
+        }
+    }
+
+    public void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Bullet go = GameObject.Instantiate(bulletPrefab, weapon.GetComponent<Transform>().position, Quaternion.identity);
+            go.SetatkValue(atkValue);
+            go.SetBulletSpeed(bulletSpeed);
+        }
+    }
+    public void PlayerMove()
+    {
+        if (Input.GetKey("w") && Input.GetKey("a"))
+        {
+            playerTransform.Translate(new Vector3(-1, 1, 0) * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("w") && Input.GetKey("d"))
+        {
+            playerTransform.Translate(new Vector3(1, 1, 0) * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("s") && Input.GetKey("d"))
+        {
+            playerTransform.Translate(new Vector3(-1, -1, 0) * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("s") && Input.GetKey("a"))
+        {
+            playerTransform.Translate(new Vector3(1, -1, 0) * speed * Time.deltaTime);
+        }
+        else
+        {
+            if (Input.GetKey("w"))
+            {
+                playerTransform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
+            }
+            else if (Input.GetKey("s"))
+            {
+                playerTransform.Translate(new Vector3(0, -1, 0) * speed * Time.deltaTime);
+            }
+            else if (Input.GetKey("a"))
+            {
+                playerTransform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+            }
+            else if (Input.GetKey("d"))
+            {
+                playerTransform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    public void LookAt()
+    {
+        Vector2 point = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        point = Camera.main.ScreenToWorldPoint(point);
+        weapon.GetComponent<Transform>().LookAt(point, Vector3.back);
+    }
+}
